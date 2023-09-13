a0a42b94a1201612519c86d9fbb4dd0030a16483
imbriankeeley <bk3700@proton.me>
2023-09-12 20:16:51 -0400

    Finished logic for all game scenarios

--- main.js
+++ main.js
@@ -12,8 +12,20 @@ function playRound(playerSelection, computerSelection){
     if (playerSelection.toLowerCase() === "rock" && computerSelection === "Scissors"){
         alert("You win! Rock beats Scissors");
     } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "Paper"){
-        alert("You Lose! Paper beats Rock");
+        alert("You lose! Paper beats Rock");
     } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "Rock"){
+        alert("It's a Tie!");
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "Rock"){
+        alert ("You win! Paper beats Rock");
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "Scissors"){
+        alert("You lose! Scissors beats Paper");
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "Paper"){
+        alert("It's a Tie!");
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "Paper"){
+        alert("You win! Scissors beats Paper");
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "Rock"){
+        alert("You lose! Rock beats Scissors");
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "Scissors"){
         alert("It's a Tie!");
     }
 };
