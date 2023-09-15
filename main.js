b215c4cd68f90c71bebf55d929f4581f440cec54
imbriankeeley <bk3700@proton.me>
2023-09-15 15:58:51 -0400

    Cleaned up logic code for playRound

--- main.js
+++ main.js
@@ -8,23 +8,11 @@ let computerScore = 0
 function playRound(playerSelection, computerSelection){
     if (playerSelection === computerSelection){
         alert("It's a Tie!");
-    } else if (playerSelection === "rock" && computerSelection === "scissors"){
+    } else if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")){
         alert("You win! Rock beats Scissors");
         playerScore ++;
-    } else if (playerSelection === "rock" && computerSelection === "paper"){
+    } else if ((playerSelection === "rock" && computerSelection === "paper") ||(playerSelection === "paper" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "rock")){
         alert("You lose! Paper beats Rock");
-        computerScore ++;
-    } else if (playerSelection === "paper" && computerSelection === "rock"){
-        alert ("You win! Paper beats Rock");
-        playerScore ++;
-    } else if (playerSelection === "paper" && computerSelection === "scissors"){
-        alert("You lose! Scissors beats Paper");
-        computerScore ++;
-    } else if (playerSelection === "scissors" && computerSelection === "paper"){
-        alert("You win! Scissors beats Paper");
-        playerScore ++
-    } else if (playerSelection === "scissors" && computerSelection === "rock"){
-        alert("You lose! Rock beats Scissors");
         computerScore ++;
     }
     return playerScore
