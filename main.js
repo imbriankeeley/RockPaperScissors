52d4ad9ad21ea522043d3d71a8ee921c8824145e
imbriankeeley <bk3700@proton.me>
2023-09-21 20:23:12 -0400

    Made logic for game and Dom steps work

--- main.js
+++ main.js
@@ -1,71 +1,94 @@
-//Player Scores
+//Variables
 let playerScore = 0
 let computerScore = 0
 
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
 
-//Game Round
-function playRound(playerSelection, computerSelection){
-    
+const scores = document.querySelector('#results');
+const playersScoreContainer = document.createElement('div');
+const playersScore = document.createElement('h1');
 
-    if (playerSelection === computerSelection){
-        alert("It's a Tie!");
-    } else if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")){
-        alert("You win! Rock beats Scissors");
-        playerScore ++;
-    } else if ((playerSelection === "rock" && computerSelection === "paper") ||(playerSelection === "paper" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "rock")){
-        alert("You lose! Paper beats Rock");
-        computerScore ++;
-    }
-    return playerScore
-    return computerScore
-};
+playersScore.classList.add('player-score');
+playersScore.textContent = `Player: ${playerScore}`
+playersScoreContainer.appendChild(playersScore);
+scores.appendChild(playersScoreContainer);
 
+const computersScoreContainer = document.createElement('div');
+const computersScore = document.createElement('h1');
 
-//Game Result
-function gameResult(){
-    if (playerScore === computerScore){
-        alert("You tied!");
-    } else if (playerScore > computerScore){
-        alert("You won!");
-    } else if (playerScore < computerScore){
-        alert("You lost!");
-    }
-};
+computersScore.classList.add('computer-score');
+computersScore.textContent = `Computer: ${computerScore}`
+computersScoreContainer.appendChild(computersScore);
+scores.appendChild(computersScoreContainer);
 
-function game(){
 //Generate Random Pick For Computer
 function getComputerChoice(){
     let decision = ["rock", "paper", "scissors"];
     return (decision[(Math.floor(Math.random() * decision.length))]);
 };
 
-const rock = document.querySelector('#rock');
-const paper = document.querySelector('#paper');
-const scissors = document.querySelector('#scissors');
+//Game Round
+function playRound(playerSelection, computerSelection){
+    
+
+    if (playerSelection === computerSelection){ 
+        playersScore.textContent = `Player:  ${playerScore}`
+        computersScore.textContent = `Computer: ${computerScore}`
+    } else if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")){
+        
+        playerScore ++;
+        playersScore.textContent = `Player:  ${playerScore}`
+        computersScore.textContent = `Computer: ${computerScore}`
+    } else if ((playerSelection === "rock" && computerSelection === "paper") ||(playerSelection === "paper" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "rock")){
+        
+        computerScore ++;
+        playersScore.textContent = `Player:  ${playerScore}`
+        computersScore.textContent = `Computer: ${computerScore}`
+    }
+
+    return playerScore
+    return computerScore
+
+    
+};
+
+//Game Result
+function gameResult(){
+    if (playerScore === 5){
+        playersScore.textContent = `You `
+        computersScore.textContent = `Win!`
+    } else if(computerScore === 5){
+        playersScore.textContent = `You `
+        computersScore.textContent = `Lose!`
+    }
+};
+
+function game(){
 
 //Play Round On Click
-for(let i = 0; i < 5; i++){
     rock.addEventListener('click', () => {
         let computerSelection = getComputerChoice();
         getComputerChoice()
         playRound('rock', computerSelection);
+        gameResult()
     });
     paper.addEventListener('click', () => {
         let computerSelection = getComputerChoice();
         getComputerChoice()
         playRound('paper', computerSelection);
+        gameResult()
     });
     scissors.addEventListener('click', () => {
         let computerSelection = getComputerChoice();
         getComputerChoice()
         playRound('scissors', computerSelection);
+        gameResult()
     });
-}
-
-
-
 
 }
+
 
 game()
 
