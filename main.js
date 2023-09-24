9a49bfc4c73630c829f635aa918e77187efc8419
imbriankeeley <imbriankeeley@gmail.com>
2023-09-23 23:57:11 -0400

    Added buttono transformations and reset functionality

--- main.js
+++ main.js
@@ -23,6 +23,7 @@ computersScore.textContent = `Computer: ${computerScore}`
 computersScoreContainer.appendChild(computersScore);
 scores.appendChild(computersScoreContainer);
 
+
 //Generate Random Pick For Computer
 function getComputerChoice(){
     let decision = ["rock", "paper", "scissors"];
@@ -31,21 +32,27 @@ function getComputerChoice(){
 
 //Game Round
 function playRound(playerSelection, computerSelection){
-    
+    const status = document.querySelector('.status');
 
     if (playerSelection === computerSelection){ 
+
         playersScore.textContent = `Player:  ${playerScore}`
         computersScore.textContent = `Computer: ${computerScore}`
+        status.textContent = 'Bummer, it\'s a tie'
     } else if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")){
         
         playerScore ++;
         playersScore.textContent = `Player:  ${playerScore}`
         computersScore.textContent = `Computer: ${computerScore}`
+        status.textContent = 'You won this round!'
+
     } else if ((playerSelection === "rock" && computerSelection === "paper") ||(playerSelection === "paper" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "rock")){
         
         computerScore ++;
         playersScore.textContent = `Player:  ${playerScore}`
         computersScore.textContent = `Computer: ${computerScore}`
+        status.textContent = 'You lost this round :('
+
     }
 
     return playerScore
@@ -56,12 +63,19 @@ function playRound(playerSelection, computerSelection){
 
 //Game Result
 function gameResult(){
+    const status = document.querySelector('.status');
+
+
     if (playerScore === 5){
         playersScore.textContent = `You `
         computersScore.textContent = `Win!`
+        status.textContent = 'Congratulations! Play again?'
+
     } else if(computerScore === 5){
         playersScore.textContent = `You `
         computersScore.textContent = `Lose!`
+        status.textContent = 'Play again?'
+        
     }
 };
 
@@ -89,6 +103,17 @@ function game(){
 
 }
 
+const resetButton = document.querySelector('.reset');
+resetButton.addEventListener('click', () => {
+    const status = document.querySelector('.status');
+
+    let playerScore = 0;
+    let computerScore = 0;
+    playersScore.textContent = `Player: ${playerScore}`
+    computersScore.textContent = `Computer: ${computerScore}`
+    status.textContent = 'Chose your hand'
+
+});
 
 game()
 
